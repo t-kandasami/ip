0b1b86e5f0cac1446d4e237a46ae41f0b0117dfe
t-kandasami <t.kandasami1@gmail.com>
2025-08-27 20:04:08 +0800

    Add Task Class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setAsDone() {
+        this.isDone = true;
+    }
+
+    public void setAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (getDescription());
+    }
+}
