e9ad4d3c0b618d0e9e4b1cd3e99d72eff4048a6e
T. Kandasami <t.kandasami1@gmail.com>
2025-09-03 18:57:25 +0800

    Update Prime.java
    * Added the creation Deadline, ToDo, Event functionalities.
    * Created parsing methods to ensure the proper retrieval of proper informaion for each type of Task.
    * Refactored unmark and mark functions to use a single function called toggleMarkTask

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -13,12 +13,37 @@ public class Prime {
         System.out.println("    " + message);
     }
 
-    // Add a task to list
-    private static void addTask(String description) {
-        if (taskCounter < MAX_TASKS && description != null) {
-            tasksList[taskCounter] = new Task(description);
+    private static void addDeadline (String description, String by) {
+        if (taskCounter < MAX_TASKS && description != null && by != null) {
+            tasksList[taskCounter] = new Deadline(description, by);
+            printIndented("Got it. I've added this task:");
+            printIndented(tasksList[taskCounter].toString());
             taskCounter++;
-            printIndented("added: " + description);
+            printIndented("Now you have " + taskCounter + " tasks in your task list.");
+        } else {
+            printIndented("Task list is full Human! Maybe its time you finish some tasks.");
+        }
+    }
+
+    private static void addEvent (String description, String from, String to) {
+        if (taskCounter < MAX_TASKS && description != null &&  from != null && to != null) {
+            tasksList[taskCounter] = new Event(description, from, to);
+            printIndented("Got it. I've added this task: \n");
+            printIndented(tasksList[taskCounter].toString());
+            taskCounter++;
+            printIndented("Now you have " + taskCounter + " tasks in your task list.");
+        } else {
+            printIndented("Task list is full Human! Maybe its time you finish some tasks.");
+        }
+    }
+
+    private static void addToDo (String description) {
+        if (taskCounter < MAX_TASKS && description != null) {
+            tasksList[taskCounter] = new ToDo(description);
+            printIndented("Got it. I've added this task: \n");
+            printIndented(tasksList[taskCounter].toString());
+            taskCounter++;
+            printIndented("Now you have " + taskCounter + " tasks in your task list.");
         } else {
             printIndented("Task list is full Human! Maybe its time you finish some tasks.");
         }
@@ -30,39 +55,60 @@ public class Prime {
             printIndented("No tasks have been added in your list yet.");
             return;
         }
-
         printIndented("Here are your tasks in your list: ");
         for (int i = 0; i < taskCounter; i++) {
             printIndented((i + 1) + "." + tasksList[i].toString());
         }
     }
 
-    // Mark a task as done
-    private static void markTask(int taskIndex) {
-        if (taskIndex >= 1 && taskIndex <= taskCounter) {
-            tasksList[taskIndex - 1].setAsDone();
-            printIndented("Nice Human! I've marked this task as done:");
-            printIndented("  " + tasksList[taskIndex - 1].toString());
+    private static void toggleMarkTask(int taskNo) {
+        if (taskNo >= 1 && taskNo <= taskCounter) {
+            if (tasksList[taskNo - 1].getIsDone()){
+                tasksList[taskNo - 1].setIsDone(false);
+                printIndented("Ok Human! I've marked this task as not done yet:");
+            } else {
+                tasksList[taskNo - 1].setIsDone(true);
+                printIndented("Nice Human! I've marked this task as done:");
+            }
+            printIndented(tasksList[taskNo - 1].toString());
         } else {
             printIndented("Invalid task number! Please try again.");
             printIndented("You are allowed to enter a number between 1 and " + taskCounter);
         }
     }
 
-    // Mark a task as not done
-    private static void unmarkTask(int taskIndex) {
-        if (taskIndex >= 1 && taskIndex <= taskCounter) {
-            tasksList[taskIndex - 1].setAsNotDone();
-            printIndented("Ok Human! I've marked this task as not done yet:");
-            printIndented("  " + tasksList[taskIndex - 1].toString());
-        } else {
-            printIndented("Invalid task number! Please try again.");
-            printIndented("You are allowed to enter a number between 1 and " + taskCounter);
-        }
+    private static String parseTodoDescription(String command) {
+        return command.substring("todo".length()).trim();
+    }
+
+    private static String parseDeadlineDescription(String command) {
+        int indexOfBy = command.indexOf("/by");
+        return command.substring("deadline".length(), indexOfBy).trim();
+    }
+
+    private static String parseEventDescription(String command) {
+        int indexOfFrom = command.indexOf("/from");
+        return command.substring("event".length(), indexOfFrom).trim();
     }
 
     private static int parseTaskNumber(String command, String prefix) {
         return Integer.parseInt(command.substring(prefix.length()).trim());
+    }
+
+    private static String parseBy(String command) {
+        int indexOfBy = command.indexOf("/by");
+        return command.substring(indexOfBy + "/by".length()).trim();
+    }
+
+    private static String parseFrom(String command) {
+        int indexOfFrom = command.indexOf("/from");
+        int indexOfTo = command.indexOf("/to");
+        return command.substring(indexOfFrom + "/from".length(), indexOfTo).trim();
+    }
+
+    private static String parseTo(String command) {
+        int indexOfTo = command.indexOf("/to");
+        return command.substring(indexOfTo + "/to".length()).trim();
     }
 
     public static void main(String[] args) {
@@ -84,25 +130,32 @@ public class Prime {
         Scanner scanner = new Scanner(System.in);
         String userInput;
         do {
-            userInput = scanner.nextLine().trim();
+            userInput = scanner.nextLine().trim().toLowerCase();
             printIndented(LINE_BREAK);
 
-            if (userInput.toLowerCase().startsWith("mark ")) {
+            if (userInput.startsWith("mark ")) {
                 int taskNumber = parseTaskNumber(userInput, "mark");
                 if (taskNumber > 0) {
-                    markTask(taskNumber);
+                    toggleMarkTask(taskNumber);
                 } else {
                     printIndented("Invalid format! Use: mark [taskNumber]");
                 }
-            } else if (userInput.toLowerCase().startsWith("unmark ")) {
+            } else if (userInput.startsWith("unmark ")) {
                 int taskNumber = parseTaskNumber(userInput, "unmark");
                 if (taskNumber > 0) {
-                    unmarkTask(taskNumber);
+                    toggleMarkTask(taskNumber);
                 } else {
                     printIndented("Invalid format! Use: unmark [taskNumber]");
                 }
-            } else {
-                switch (userInput.toLowerCase()) {
+            } else if (userInput.startsWith("todo ")) {
+                addToDo(parseTodoDescription(userInput));
+            } else if (userInput.startsWith("deadline ")) {
+                addDeadline(parseDeadlineDescription(userInput), parseBy(userInput));
+            } else if (userInput.startsWith("event ")) {
+                addEvent(parseEventDescription(userInput), parseFrom(userInput), parseTo(userInput));
+            }
+            else {
+                switch (userInput) {
                 case "bye":
                     break;
                 case "list":
@@ -112,10 +165,9 @@ public class Prime {
                     printIndented("Human, Please enter a task number:");
                     break;
                 default:
-                    addTask(userInput);
+                    addToDo(userInput);
                 }
             }
-
             if (!userInput.equalsIgnoreCase("bye")) {
                 printIndented(LINE_BREAK);
             }
