3f82195d7470daf5d9ebc4ed923b6de33abc6521
T. Kandasami <t.kandasami1@gmail.com>
2025-09-03 18:43:28 +0800

    Add Deadline Children Class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,17 @@
+public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        if (super.isDone){
+            return "[D][X] " + super.toString() + " (by: " + by + ")";
+        } else {
+            return "[D][ ] " + super.toString() + " (by: " + by + ")";
+        }
+    }
+}
