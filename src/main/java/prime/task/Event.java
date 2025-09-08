db9889a3961839c11e2c4541710419b3cc7cb590
T. Kandasami <t.kandasami1@gmail.com>
2025-09-09 00:44:56 +0800

    Create prime.task package

--- src/main/java/prime/task/Event.java
+++ src/main/java/prime/task/Event.java
@@ -1,3 +1,5 @@
+package prime.task;
+
 public class Event extends Task {
     protected String from;
     protected String to;
--- src/main/java/prime/task/ToDo.java
+++ src/main/java/prime/task/ToDo.java
@@ -1,3 +1,5 @@
+package prime.task;
+
 public class ToDo extends Task {
     public ToDo(String description) {
         super(description);
